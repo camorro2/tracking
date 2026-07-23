a7ce7c5e8208268b76a74f36cd1ffdc70c7cdf8c
camorro00 <fggyeyyyejchsysyfgd@gmail.com>
2026-07-24 00:33:30 +0200

    Create __init__.py

---
+++ darkforge/payloads/__init__.py
@@ -0,0 +1,8 @@
+# Payloads
+from .reverse_shell import ReverseShellGenerator
+from .persistence import PersistenceInstaller
+
+__all__ = [
+    'ReverseShellGenerator',
+    'PersistenceInstaller'
+]
